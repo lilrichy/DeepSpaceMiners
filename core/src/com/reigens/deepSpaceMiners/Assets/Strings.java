603baa66d26d3c9013c5490c97593f3a65cf269f
lilrichy <richardreigens@gmail.com>
2014-09-07 15:05:25 -0400

    Adding mission strings

---
+++ core/src/com/reigens/deepSpaceMiners/Assets/Strings.java
@@ -0,0 +1,43 @@
+package com.reigens.deepSpaceMiners.Assets;
+
+/**
+ * Created by Rich on 9/7/2014.
+ */
+public class Strings {
+
+    public static String level1;
+    public static String level2;
+    public static String level3;
+    public static String level4;
+    public static String level5;
+
+
+    Strings(){
+
+        //Mission Briefings
+        level1 = "Welcome to the Deep Space Miners, we sure are glad to have you. You came just in time. There has been a recently " +
+                "high demand for resources that can only be obtained in the farthest reaches of space. That's your mission, head out to " +
+                "the meteor belt at the edge of the galaxy and start gathering the smaller meteors that are on the outside." +
+                "We have assigned you a small ship that has a worm hole teleportation device that will send the meteors back to our processing facility. " +
+                "Once you get enough credits you can work on getting a bigger and better ship to go after the high paying meteors. " +
+                "Good luck out there!";
+
+        level2 = "Coming Soon!";
+
+        level3 = "Coming Soon!";
+
+        level4 = "Coming Soon!";
+
+        level5 = "Coming Soon!";
+
+
+
+
+        //Completed Mission
+
+        //Failed Missions
+
+
+
+    }
+}
