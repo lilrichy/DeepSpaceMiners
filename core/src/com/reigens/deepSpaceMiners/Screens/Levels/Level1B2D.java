109c683fc1992a65ce812f39676b9e48c29dc1a3
lilrichy <richardreigens@gmail.com>
2014-09-12 00:31:32 -0400

    Fixed some file names

---
+++ core/src/com/reigens/deepSpaceMiners/Screens/Levels/Level1B2D.java
@@ -0,0 +1,51 @@
+package com.reigens.deepSpaceMiners.Screens.Levels;
+
+import com.badlogic.gdx.Screen;
+import com.reigens.deepSpaceMiners.GameMain;
+
+/**
+ * Created by Richard Reigens on 9/12/2014.
+ */
+public class Level1B2D implements Screen {
+    GameMain game;
+
+    public Level1B2D(GameMain game){
+        this.game = game;
+
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+     public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
